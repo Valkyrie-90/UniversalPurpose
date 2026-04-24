7f1e9c6137c73372c41b63b8572be2572d930160
Valkyrie-90 <admin@nexix.org>
2026-04-24 21:52:01 +0100

    update tsconfig, package & imports

--- src/permissions/botMaster.ts
+++ src/permissions/botMaster.ts
@@ -1,10 +1,5 @@
-<<<<<<< HEAD
 import { GuildMember, Interaction } from "discord.js";
-import { readServerConfig } from "../utils/jsonhelpers/readServerConfig";
-=======
-import { Interaction } from "discord.js";
 import { env } from "@up/main/schema";
->>>>>>> 9ae3529 (update tsconfig, package & imports)
 
 function isBotMaster(interaction: Interaction, userId: string): boolean {
     if (!interaction.guild) return false;
@@ -12,22 +7,7 @@ function isBotMaster(interaction: Interaction, userId: string): boolean {
     const member = interaction.guild.members.cache.get(userId) ?? (interaction.member as GuildMember);
 
     if (!member || !member.roles?.cache) return false;
-<<<<<<< HEAD
-
-    const serverConfig = readServerConfig(interaction.guild.id);
-    if (!serverConfig) {
-        return false;
-    }
-
-    const botMasterRoleID = serverConfig.botMasterRoleID;
-    if (botMasterRoleID && member.roles.cache.has(botMasterRoleID)) {
-        return true;
-    }
-
-    return false;
-=======
     return member.roles.cache.some((r: { name: string; }) => r.name === env.BOT_MASTER_ROLE_NAME);
->>>>>>> 9ae3529 (update tsconfig, package & imports)
 }
 
 export { isBotMaster };
