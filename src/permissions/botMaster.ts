7552f6daea2406f5449e231e7ba88a920c076fca
Daniel Brain <204735635+Valkyrie-90@users.noreply.github.com>
2026-04-25 00:46:16 +0100

    fix catostrophic git failure
    Signed-off-by: Daniel Brain <204735635+Valkyrie-90@users.noreply.github.com>

--- src/permissions/botMaster.ts
+++ src/permissions/botMaster.ts
@@ -1,15 +1,5 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-import { GuildMember, Interaction } from "discord.js";
-import { readServerConfig } from "../utils/jsonhelpers/readServerConfig";
-=======
 import { Interaction } from "discord.js";
 import { env } from "@up/main/schema";
->>>>>>> 9ae3529 (update tsconfig, package & imports)
-=======
-import { Interaction } from "discord.js";
-import { env } from "@up/main/schema";
->>>>>>> dc093e43ad5cfc99fbd9d76b70bae1caacc94120
 
 function isBotMaster(interaction: Interaction, userId: string): boolean {
     if (!interaction.guild) return false;
@@ -17,8 +7,6 @@ function isBotMaster(interaction: Interaction, userId: string): boolean {
     const member = interaction.guild.members.cache.get(userId) ?? (interaction.member as GuildMember);
 
     if (!member || !member.roles?.cache) return false;
-<<<<<<< HEAD
-<<<<<<< HEAD
 
     const serverConfig = readServerConfig(interaction.guild.id);
     if (!serverConfig) {
@@ -30,13 +18,7 @@ function isBotMaster(interaction: Interaction, userId: string): boolean {
         return true;
     }
 
-    return false;
-=======
     return member.roles.cache.some((r: { name: string; }) => r.name === env.BOT_MASTER_ROLE_NAME);
->>>>>>> 9ae3529 (update tsconfig, package & imports)
-=======
-    return member.roles.cache.some((r: { name: string; }) => r.name === env.BOT_MASTER_ROLE_NAME);
->>>>>>> dc093e43ad5cfc99fbd9d76b70bae1caacc94120
 }
 
 export { isBotMaster };
